f7b55d9a02b6bc02e3ae5e20f09024ddf17544c9
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-11-15 13:26:11 +0100

    M.

--- xml/element_test.go
+++ xml/element_test.go
@@ -18,5 +18,29 @@ func TestElementNameAndNamespace(t *testing.T) {
 }
 
 func TestShadowCopy(t *testing.T) {
+	e1 := NewElementNamespace("iq", "jabber:client")
+	e1.AppendElement(NewElementNamespace("query", "im.jackal"))
+	e1.SetText("a text")
 
+	e2 := Element{e1.p, 0}
+	e3 := Element{e1.p, 0}
+	e4 := Element{e1.p, 0}
+	e5 := Element{e1.p, 0}
+
+	e2.SetName("message")
+	if e1.shared() == e2.shared() {
+		t.Error("e1.p == e2.p after setting name")
+	}
+	e3.SetText("another text")
+	if e1.shared() == e3.shared() {
+		t.Error("e1.p == e3.p after setting text")
+	}
+	e4.SetAttribute("id", "abcde")
+	if e1.shared() == e4.shared() {
+		t.Error("e1.p == e4.p after setting attribute")
+	}
+	e5.AppendElement(NewElementName("item"))
+	if e1.shared() == e5.shared() {
+		t.Error("e1.p == e5.p after appending element")
+	}
 }
