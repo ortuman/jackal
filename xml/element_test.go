f6605f919326a791a94fe0ee824bb08a5812ace0
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-11-17 10:06:03 +0100

    M.

--- xml/element_test.go
+++ xml/element_test.go
@@ -8,17 +8,15 @@ package xml_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/ortuman/jackal/xml"
 )
 
 func TestElementNameAndNamespace(t *testing.T) {
 	e := xml.NewElementNamespace("n", "ns")
-	if e.Name() != "n" {
-		t.Fatalf("wrong name: '%s'. expected 'n'", e.Name())
-	}
-	if e.Namespace() != "ns" {
-		t.Fatalf("wrong namespace: '%s'. expected 'ns'", e.Namespace())
-	}
+	assert.Equal(t, e.Name(), "n")
+	assert.Equal(t, e.Namespace(), "ns")
 }
 
 func TestAttribute(t *testing.T) {
@@ -27,16 +25,9 @@ func TestAttribute(t *testing.T) {
 	e.SetLanguage("en")
 	e.SetVersion("1.0")
 
-	if e.ID() != "123" {
-		t.Fatalf("id == %s. expected 123", e.ID())
-	}
-	if e.AttributesCount() != 3 {
-		t.Fatalf("attributes count == %d. expected 3.", e.AttributesCount())
-	}
-	e.RemoveAttribute("xml:lang")
-	if e.AttributesCount() != 2 {
-		t.Fatalf("attributes count == %d. expected 2.", e.AttributesCount())
-	}
+	assert.Equal(t, e.ID(), "123")
+	assert.Equal(t, e.Language(), "en")
+	assert.Equal(t, e.Version(), "1.0")
 }
 
 func TestElement(t *testing.T) {
@@ -48,41 +39,29 @@ func TestElement(t *testing.T) {
 	e.AppendElement(xml.NewElementNamespace("c", "ns3"))
 	e.AppendElement(xml.NewElementName("d"))
 	a := e.FindElement("a")
-	if a == nil {
-		t.Fatal("a == nil")
-	}
+	assert.NotNil(t, a)
+
 	c := e.FindElements("c")
-	if len(c) != 3 {
-		t.Fatalf("len(c) != %d. expected 3", len(c))
-	}
+	assert.Equal(t, len(c), 3)
+
 	c1 := e.FindElementsNamespace("c", "ns1")
-	if len(c1) != 1 {
-		t.Fatalf("len(c1) == %d. expected 1", len(c1))
-	}
-	count := e.ElementsCount()
-	if count != 6 {
-		t.Fatalf("count == %d. expected 6", count)
-	}
+	assert.Equal(t, len(c1), 1)
+	assert.Equal(t, e.ElementsCount(), 6)
 }
 
 func TestCopy(t *testing.T) {
 	e := xml.NewMutableElementName("n")
-	e.SetID("123")
+	e.SetAttribute("att", "val")
+	e.SetText("a text")
 	e.AppendElement(xml.NewElementName("a"))
 	cp := e.Copy()
 
-	if cp.Name() != e.Name() {
-		t.Fatal("cp.Name() != e.Name()")
-	}
-	if cp.ID() != e.ID() {
-		t.Fatal("cp.ID() != e.ID() ")
-	}
-	if cp.ElementsCount() != e.ElementsCount() {
-		t.Fatal("cp.ElementsCount() != e.ElementsCount()")
-	}
-	cpElems := cp.Elements()
-	eElems := e.Elements()
-	if cpElems[0] != eElems[0] {
-		t.Fatal("cpElems[0] != eElems[0]")
-	}
+	assert.NotEqual(t, cp.ElementsCount(), 0)
+	assert.NotEqual(t, e.ElementsCount(), 0)
+	assert.Equal(t, cp.Name(), e.Name())
+	assert.Equal(t, cp.Text(), e.Text())
+	assert.Equal(t, cp.AttributesCount(), e.AttributesCount())
+	assert.Equal(t, cp.Attribute("att"), e.Attribute("att"))
+	assert.Equal(t, cp.ElementsCount(), e.ElementsCount())
+	assert.Equal(t, cp.Elements()[0], e.Elements()[0])
 }
--- xml/mutable_element_test.go
+++ xml/mutable_element_test.go
@@ -8,24 +8,83 @@ package xml_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/ortuman/jackal/xml"
 )
 
 func TestSetName(t *testing.T) {
 	m := xml.NewMutableElementName("n")
-	m.SetName("new_n")
-	if m.Name() != "new_n" {
-		t.Fatal(`m.Name() != "new_n"`)
-	}
+	m.SetName("n2")
+	assert.Equal(t, m.Name(), "n2")
 }
 
 func TestSetText(t *testing.T) {
 	m := xml.NewMutableElementName("n")
 	m.SetText("example text")
-	if m.Text() != "example text" {
-		t.Fatal(`m.Text() != "example text"`)
-	}
+	assert.Equal(t, m.Text(), "example text")
+}
+
+func TestSetAttribute(t *testing.T) {
+	m := xml.NewMutableElementName("n")
+	m.SetAttribute("att", "val")
+	assert.Equal(t, m.Attribute("att"), "val")
+
+	// replace attribute
+	m.SetAttribute("att", "val2")
+	assert.Equal(t, m.Attribute("att"), "val2")
+}
+
+func TestRemoveAttribute(t *testing.T) {
+	m := xml.NewMutableElementName("n")
+	m.SetAttribute("att", "val")
+	m.SetAttribute("att2", "val2")
+	assert.Equal(t, m.AttributesCount(), 2)
+
+	m.RemoveAttribute("att")
+	assert.Equal(t, m.AttributesCount(), 1)
+	assert.Empty(t, m.Attribute("att"))
+	assert.Equal(t, m.Attribute("att2"), "val2")
+
+	m.RemoveAttribute("att2")
+	assert.Equal(t, m.AttributesCount(), 0)
+}
+
+func TestRemoveElements(t *testing.T) {
+	m := xml.NewMutableElementName("n")
+	m.AppendElement(xml.NewElementName("a"))
+	m.AppendElement(xml.NewElementNamespace("b", "ns1"))
+	m.AppendElement(xml.NewElementNamespace("b", "ns2"))
+	m.AppendElement(xml.NewElementNamespace("b", "ns3"))
+	m.AppendElement(xml.NewElementName("c"))
+	assert.Equal(t, m.ElementsCount(), 5)
+
+	m.RemoveElementsNamespace("b", "ns1")
+	assert.Equal(t, m.ElementsCount(), 4)
+
+	b2 := m.FindElementNamespace("b", "ns2")
+	assert.NotNil(t, b2)
+
+	m.RemoveElements("b")
+	assert.Equal(t, m.ElementsCount(), 2)
+
+	m.ClearElements()
+	assert.Equal(t, m.ElementsCount(), 0)
 }
 
 func TestMutableCopy(t *testing.T) {
+	m := xml.NewMutableElementName("n")
+	m.SetAttribute("att", "val")
+	m.SetText("a text")
+	m.AppendElement(xml.NewElementName("a"))
+	cp := m.MutableCopy()
+
+	assert.NotEqual(t, cp.ElementsCount(), 0)
+	assert.NotEqual(t, m.ElementsCount(), 0)
+	assert.Equal(t, cp.Name(), m.Name())
+	assert.Equal(t, cp.Text(), m.Text())
+	assert.Equal(t, cp.AttributesCount(), m.AttributesCount())
+	assert.Equal(t, cp.Attribute("att"), m.Attribute("att"))
+	assert.Equal(t, cp.ElementsCount(), m.ElementsCount())
+	assert.Equal(t, cp.Elements()[0], m.Elements()[0])
 }
