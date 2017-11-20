a696e158d90feaa2c312b1f53ab2b95c59850c7c
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-11-20 16:53:11 +0100

    M.

--- xml/parser_test.go
+++ xml/parser_test.go
@@ -32,3 +32,17 @@ func TestFailedDocParse(t *testing.T) {
 	err := p.ParseElements(strings.NewReader(docSrc))
 	assert.NotNil(t, err)
 }
+
+func TestDocChildElements(t *testing.T) {
+	docSrc := `<?xml version="1.0" encoding="UTF-8"?>\n<parent><a/><b/><c/></parent>\n`
+	p := xml.NewParser()
+	err := p.ParseElements(strings.NewReader(docSrc))
+	assert.Nil(t, err)
+	parent := p.PopElement()
+	assert.NotNil(t, parent)
+	childs := parent.Elements()
+	assert.Equal(t, len(childs), 3)
+	assert.Equal(t, childs[0].Name(), "a")
+	assert.Equal(t, childs[1].Name(), "b")
+	assert.Equal(t, childs[2].Name(), "c")
+}
