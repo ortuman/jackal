14a1f1982a169b85f8df50afe5b17301b79b7bb6
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-11-21 20:24:07 +0100

    M.

--- main.go
+++ main.go
@@ -5,17 +5,40 @@
 
 package main
 
-import "github.com/ortuman/jackal/config"
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ortuman/jackal/config"
+	"github.com/ortuman/jackal/version"
+)
 
 func main() {
-	if err := config.Load("jackal.yaml"); err != nil {
-		fmt.Printf("%v", err)
+	var configFile string
+	var showVersion bool
+	var showUsage bool
+
+	flag.BoolVar(&showUsage, "help", false, "show application usage")
+	flag.BoolVar(&showVersion, "version", false, "show application version")
+	flag.StringVar(&configFile, "config", "/etc/jackal/jackal.yaml", "configuration path file")
+	flag.Parse()
+
+	// print usage
+	if showUsage {
+		flag.Usage()
+		os.Exit(-1)
 	}
-	d := config.DefaultConfig
-	println(d.PIDFile)
-}
 
-func printVersion() {
+	// print version
+	if showVersion {
+		fmt.Printf("jackal version: %v", version.ApplicationVersion)
+		os.Exit(-1)
+	}
 
+	// load configuration
+	if err := config.Load(configFile); err != nil {
+		fmt.Fprintf(os.Stderr, "jackal: %v", err)
+		os.Exit(-1)
+	}
 }
