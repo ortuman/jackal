52360a24f0073ce434912ced927457ea921b1958
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-11-22 12:07:21 +0100

    M.

--- server/server.go
+++ server/server.go
@@ -6,9 +6,38 @@
 package server
 
 import (
-	"time"
+	"github.com/ortuman/jackal/config"
 )
 
+const (
+	// C2S represents a client to client server type.
+	C2S = iota
+	// S2S represents a server-to-client server type.
+	S2S
+)
+
+type server struct {
+	cfg *config.Server
+}
+
 func Initialize() {
-	time.Sleep(time.Second * 3)
+	for i := 1; i < len(config.DefaultConfig.Servers); i++ {
+		go initializeServer(&config.DefaultConfig.Servers[i])
+	}
+	initializeServer(&config.DefaultConfig.Servers[0])
+}
+
+func initializeServer(serverConfig *config.Server) {
+	srv := newServerWithConfig(serverConfig)
+	srv.start()
+}
+
+func newServerWithConfig(serverConfig *config.Server) *server {
+	s := &server{
+		cfg: serverConfig,
+	}
+	return s
+}
+
+func (s *server) start() {
 }
