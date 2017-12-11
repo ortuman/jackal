67da2100e19fc64c4cef5e0583a5d41aa60f1134
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-12-11 19:04:33 +0100

    M.

---
+++ concurrent/dispatcher_test.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017-2018 Miguel Ángel Ortuño.
+ * See the COPYING file for more information.
+ */
+
+package concurrent_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ortuman/jackal/concurrent"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAsync(t *testing.T) {
+	queue := concurrent.DispatcherQueue{}
+	var v int
+	for i := 0; i < 128; i++ {
+		queue.Async(func() {
+			v++
+		})
+	}
+	for i := 0; i < 128; i++ {
+		queue.Async(func() {
+			v++
+		})
+	}
+	time.Sleep(time.Second)
+	assert.Equal(t, v, 256)
+}
