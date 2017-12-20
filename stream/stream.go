603d42e5cde5187608ed0c0983c378d015076bf0
Miguel Ángel Ortuño <ortuman@gmail.com>
2017-12-20 17:47:07 +0100

    M.

--- stream/stream.go
+++ stream/stream.go
@@ -748,6 +748,7 @@ func (s *Stream) buildStanza(elem *xml.Element) (xml.Stanza, error) {
 			return nil, xml.ErrBadRequest
 		}
 		return iq, nil
+
 	case "presence":
 		presence, err := xml.NewPresence(elem, fromJID, toJID)
 		if err != nil {
@@ -755,6 +756,7 @@ func (s *Stream) buildStanza(elem *xml.Element) (xml.Stanza, error) {
 			return nil, xml.ErrBadRequest
 		}
 		return presence, nil
+
 	case "message":
 		message, err := xml.NewMessage(elem, fromJID, toJID)
 		if err != nil {
