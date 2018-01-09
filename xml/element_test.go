f297922036992d207559b210f9201020cbd8fca8
Miguel Ángel Ortuño <ortuman@gmail.com>
2018-01-09 16:58:59 +0100

    M.

--- xml/element_test.go
+++ xml/element_test.go
@@ -20,7 +20,7 @@ func TestElementNameAndNamespace(t *testing.T) {
 }
 
 func TestAttribute(t *testing.T) {
-	e := xml.NewMutableElementName("n")
+	e := xml.NewElementName("n")
 	e.SetID("123")
 	e.SetLanguage("en")
 	e.SetVersion("1.0")
@@ -31,7 +31,7 @@ func TestAttribute(t *testing.T) {
 }
 
 func TestElement(t *testing.T) {
-	e := xml.NewMutableElementName("n")
+	e := xml.NewElementName("n")
 	e.AppendElement(xml.NewElementName("a"))
 	e.AppendElement(xml.NewElementName("b"))
 	e.AppendElement(xml.NewElementNamespace("c", "ns1"))
@@ -47,21 +47,4 @@ func TestElement(t *testing.T) {
 	c1 := e.FindElementsNamespace("c", "ns1")
 	assert.Equal(t, len(c1), 1)
 	assert.Equal(t, e.ElementsCount(), 6)
-}
-
-func TestCopy(t *testing.T) {
-	e := xml.NewMutableElementName("n")
-	e.SetAttribute("att", "val")
-	e.SetText("a text")
-	e.AppendElement(xml.NewElementName("a"))
-	cp := e.Copy()
-
-	assert.NotEqual(t, cp.ElementsCount(), 0)
-	assert.NotEqual(t, e.ElementsCount(), 0)
-	assert.Equal(t, cp.Name(), e.Name())
-	assert.Equal(t, cp.Text(), e.Text())
-	assert.Equal(t, cp.AttributesCount(), e.AttributesCount())
-	assert.Equal(t, cp.Attribute("att"), e.Attribute("att"))
-	assert.Equal(t, cp.ElementsCount(), e.ElementsCount())
-	assert.Equal(t, cp.Elements()[0], e.Elements()[0])
 }
