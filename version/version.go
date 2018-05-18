ef099775b8c11ce63e03c08479fad48d03c82d7b
Miguel Ángel Ortuño <ortuman@gmail.com>
2018-05-18 14:25:04 +0200

    Update version.go

--- version/version.go
+++ version/version.go
@@ -10,7 +10,7 @@ import (
 )
 
 // ApplicationVersion represents application version.
-var ApplicationVersion = NewVersion(0, 2, 1)
+var ApplicationVersion = NewVersion(0, 2, 0)
 
 // SemanticVersion represents version information with Semantic Versioning specifications.
 type SemanticVersion struct {
