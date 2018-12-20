fb6c0223f758dde416582992ddf0f87d7b6d6e28
Miguel Ángel Ortuño <ortuman@gmail.com>
2018-12-20 17:17:44 +0100

    updated README.md

--- cluster/event_delegate.go
+++ cluster/event_delegate.go
@@ -11,12 +11,6 @@ type memberListEventDelegate struct {
 	cluster *Cluster
 }
 
-func (d *memberListEventDelegate) NotifyJoin(n *memberlist.Node) {
-	d.cluster.handleNotifyJoin(n)
-}
-
-func (d *memberListEventDelegate) NotifyLeave(n *memberlist.Node) {
-	d.cluster.handleNotifyLeave(n)
-}
-
+func (d *memberListEventDelegate) NotifyJoin(n *memberlist.Node)   { d.cluster.handleNotifyJoin(n) }
+func (d *memberListEventDelegate) NotifyLeave(n *memberlist.Node)  { d.cluster.handleNotifyLeave(n) }
 func (d *memberListEventDelegate) NotifyUpdate(n *memberlist.Node) {}
