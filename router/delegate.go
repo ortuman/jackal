c09462b4f194d5b967c5e6acdced2be261c37072
Miguel Ángel Ortuño <ortuman@gmail.com>
2018-12-20 16:19:58 +0100

    M.

--- router/delegate.go
+++ router/delegate.go
@@ -13,14 +13,6 @@ type clusterDelegate struct {
 	r *Router
 }
 
-func (d *clusterDelegate) NotifyMessage(msg *cluster.Message) {
-	d.r.handleNotifyMessage(msg)
-}
-
-func (d *clusterDelegate) NodeJoined(node *cluster.Node) {
-	d.r.handleNodeJoined(node)
-}
-
-func (d *clusterDelegate) NodeLeft(node *cluster.Node) {
-	d.r.handleNodeLeft(node)
-}
+func (d *clusterDelegate) NotifyMessage(msg *cluster.Message) { d.r.handleNotifyMessage(msg) }
+func (d *clusterDelegate) NodeJoined(node *cluster.Node)      { d.r.handleNodeJoined(node) }
+func (d *clusterDelegate) NodeLeft(node *cluster.Node)        { d.r.handleNodeLeft(node) }
