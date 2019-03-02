1dc0a1794b968994c115db88c89a8ed8528dd336
ortuman <ortuman@gmail.com>
2019-03-02 16:17:23 +0100

    M.

--- router/router.go
+++ router/router.go
@@ -7,7 +7,6 @@ package router
 
 import (
 	"crypto/tls"
-	"errors"
 	"runtime"
 	"sync"
 
@@ -24,28 +23,6 @@ import (
 const defaultDomain = "localhost"
 
 var bindMsgBatchSize = 1024
-
-var (
-	// ErrNotExistingAccount will be returned by Route method
-	// if destination user does not exist.
-	ErrNotExistingAccount = errors.New("router: account does not exist")
-
-	// ErrResourceNotFound will be returned by Route method
-	// if destination resource does not match any of user's available resources.
-	ErrResourceNotFound = errors.New("router: resource not found")
-
-	// ErrNotAuthenticated will be returned by Route method if
-	// destination user is not available at this moment.
-	ErrNotAuthenticated = errors.New("router: user not authenticated")
-
-	// ErrBlockedJID will be returned by Route method if
-	// destination jid matches any of the user's blocked jid.
-	ErrBlockedJID = errors.New("router: destination jid is blocked")
-
-	// ErrFailedRemoteConnect will be returned by Route method if
-	// couldn't establish a connection to the remote server.
-	ErrFailedRemoteConnect = errors.New("router: failed remote connection")
-)
 
 // OutS2SProvider provides a specific s2s outgoing connection for every single
 // pair of (localdomain, remotedomain) values.
