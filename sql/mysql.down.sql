8ee7a0b874f4e9b7a8dc993718ab35b41ef64238
ortuman <ortuman@gmail.com>
2019-03-02 19:41:51 +0100

    postgres pubsub sql scheme

--- sql/mysql.down.sql
+++ sql/mysql.down.sql
@@ -3,8 +3,10 @@
  * See the LICENSE file for more information.
  */
 
-DROP TABLE IF EXISTS pep_subscriptions;
-DROP TABLE IF EXISTS pep_nodes;
+DROP TABLE IF EXISTS pubsub_items;
+DROP TABLE IF EXISTS pubsub_affiliations;
+DROP TABLE IF EXISTS pubsub_node_options;
+DROP TABLE IF EXISTS pubsub_nodes;
 DROP TABLE IF EXISTS offline_messages;
 DROP TABLE IF EXISTS vcards;
 DROP TABLE IF EXISTS private_storage;
