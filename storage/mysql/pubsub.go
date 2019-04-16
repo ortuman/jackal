b4152dc97120a1f2197650b25f91d4ca7c083f60
Miguel Ángel Ortuño <ortuman@gmail.com>
2019-04-16 18:19:28 +0200

    pubsub node create

--- storage/mysql/pubsub.go
+++ storage/mysql/pubsub.go
@@ -1,11 +1,44 @@
 package mysql
 
 import (
-	"errors"
+	"database/sql"
 
+	sq "github.com/Masterminds/squirrel"
 	pubsubmodel "github.com/ortuman/jackal/model/pubsub"
 )
 
-func (m *Storage) InsertOrUpdatePubSubNode(node *pubsubmodel.Node) error {
-	return errors.New("unimplemented method")
+func (s *Storage) InsertOrUpdatePubSubNode(node *pubsubmodel.Node) error {
+	return s.inTransaction(func(tx *sql.Tx) error {
+		// if not existing, insert new node
+		res, err := sq.Insert("pubsub_nodes").
+			Columns("host", "name", "updated_at", "created_at").
+			Suffix("ON DUPLICATE KEY UPDATE updated_at = NOW()").
+			Values(node.Host, node.Name, nowExpr, nowExpr).
+			RunWith(tx).Exec()
+		if err != nil {
+			return err
+		}
+		nodeIdentifier, err := res.LastInsertId()
+		if err != nil {
+			return err
+		}
+		// delete previous node options
+		_, err = sq.Delete("pubsub_node_options").
+			Where(sq.Eq{"node_id": nodeIdentifier}).
+			RunWith(tx).Exec()
+		if err != nil {
+			return err
+		}
+		// insert new option set
+		for _, opt := range node.Options {
+			_, err = sq.Insert("pubsub_node_options").
+				Columns("node_id", "name", "value").
+				Values(nodeIdentifier, opt.Name, opt.Value).
+				RunWith(tx).Exec()
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
--- storage/pubsub.go
+++ storage/pubsub.go
@@ -5,3 +5,7 @@ import pubsubmodel "github.com/ortuman/jackal/model/pubsub"
 type pubSubStorage interface {
 	InsertOrUpdatePubSubNode(node *pubsubmodel.Node) error
 }
+
+func InsertOrUpdatePubSubNode(node *pubsubmodel.Node) error {
+	return inst.InsertOrUpdatePubSubNode(node)
+}
