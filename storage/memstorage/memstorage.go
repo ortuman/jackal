509395199330a6b1e7511a9dbe3dbcfdb55cc249
Miguel Ángel Ortuño <ortuman@gmail.com>
2019-07-27 11:47:37 +0200

    implemented serializer interface

--- storage/memstorage/memstorage.go
+++ storage/memstorage/memstorage.go
@@ -8,10 +8,6 @@ package memstorage
 import (
 	"errors"
 	"sync"
-
-	"github.com/ortuman/jackal/model"
-	"github.com/ortuman/jackal/model/rostermodel"
-	"github.com/ortuman/jackal/xmpp"
 )
 
 // ErrMockedError will be returned by any Storage method
@@ -20,35 +16,18 @@ var ErrMockedError = errors.New("memstorage: mocked error")
 
 // Storage represents an in memory storage sub system.
 type Storage struct {
-	mockErrMu           sync.Mutex
-	mockingErr          bool
-	invokeLimit         int32
-	invokeCount         int32
-	mu                  sync.RWMutex
-	users               map[string]*model.User
-	rosterItems         map[string][]rostermodel.Item
-	rosterVersions      map[string]rostermodel.Version
-	rosterNotifications map[string][]rostermodel.Notification
-	vCards              map[string]xmpp.XElement
-	privateXML          map[string][]xmpp.XElement
-	offlineMessages     map[string][]xmpp.Message
-	blockListItems      map[string][]model.BlockListItem
-
-	bytes map[string][]byte
+	mockErrMu   sync.Mutex
+	mockingErr  bool
+	invokeLimit int32
+	invokeCount int32
+	mu          sync.RWMutex
+	bytes       map[string][]byte
 }
 
 // New returns a new in memory storage instance.
 func New() *Storage {
 	return &Storage{
-		users:               make(map[string]*model.User),
-		rosterItems:         make(map[string][]rostermodel.Item),
-		rosterVersions:      make(map[string]rostermodel.Version),
-		rosterNotifications: make(map[string][]rostermodel.Notification),
-		vCards:              make(map[string]xmpp.XElement),
-		privateXML:          make(map[string][]xmpp.XElement),
-		offlineMessages:     make(map[string][]xmpp.Message),
-		blockListItems:      make(map[string][]model.BlockListItem),
-		bytes:               make(map[string][]byte),
+		bytes: make(map[string][]byte),
 	}
 }
 
