ea3c08ebf723b063e25470cb7d4b2a670c521f6a
Miguel Ángel Ortuño <ortuman@gmail.com>
2019-07-27 14:00:46 +0200

    implementing PEP

--- storage/memstorage/pubsub.go
+++ storage/memstorage/pubsub.go
@@ -1,13 +1,37 @@
 package memstorage
 
-import pubsubmodel "github.com/ortuman/jackal/model/pubsub"
+import (
+	pubsubmodel "github.com/ortuman/jackal/model/pubsub"
+	"github.com/ortuman/jackal/model/serializer"
+)
 
 func (m *Storage) UpsertPubSubNode(node *pubsubmodel.Node) error {
-	return nil
+	b, err := serializer.Serialize(node)
+	if err != nil {
+		return err
+	}
+	return m.inWriteLock(func() error {
+		m.bytes[pubSubNodeKey(node.Host, node.Name)] = b
+		return nil
+	})
 }
 
 func (m *Storage) FetchPubSubNode(host, name string) (*pubsubmodel.Node, error) {
-	return nil, nil
+	var b []byte
+	if err := m.inReadLock(func() error {
+		b = m.bytes[pubSubNodeKey(host, name)]
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	if b == nil {
+		return nil, nil
+	}
+	var node pubsubmodel.Node
+	if err := serializer.Deserialize(b, &node); err != nil {
+		return nil, err
+	}
+	return &node, nil
 }
 
 func (m *Storage) UpsertPubSubNodeItem(item *pubsubmodel.Item, host, name string, maxNodeItems int) error {
@@ -23,4 +47,8 @@ func (m *Storage) UpsertPubSubNodeAffiliation(affiliation *pubsubmodel.Affiliati
 
 func (m *Storage) FetchPubSubNodeAffiliations(host, name string) ([]pubsubmodel.Affiliation, error) {
 	return nil, nil
+}
+
+func pubSubNodeKey(host, name string) string {
+	return "pubSubNodes:" + host + ":" + name
 }
