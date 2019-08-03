1984a7d2a6b36d12b07465e2977ff524ba651b9f
Miguel Ángel Ortuño <ortuman@gmail.com>
2019-08-03 19:25:15 +0200

    finished memory storage pubsub

--- storage/memstorage/pubsub.go
+++ storage/memstorage/pubsub.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 Miguel Ángel Ortuño.
+ * See the LICENSE file for more information.
+ */
+
 package memstorage
 
 import (
@@ -35,21 +40,114 @@ func (m *Storage) FetchPubSubNode(host, name string) (*pubsubmodel.Node, error) 
 }
 
 func (m *Storage) UpsertPubSubNodeItem(item *pubsubmodel.Item, host, name string, maxNodeItems int) error {
-	return nil
+	return m.inWriteLock(func() error {
+		var b []byte
+		var items []pubsubmodel.Item
+
+		b = m.bytes[pubSubItemsKey(host, name)]
+		if b != nil {
+			if err := serializer.DeserializeSlice(b, &items); err != nil {
+				return err
+			}
+		}
+		var updated bool
+		for i, itm := range items {
+			if itm.ID == item.ID {
+				items[i] = *item
+				updated = true
+				break
+			}
+		}
+		if !updated {
+			items = append(items, *item)
+		}
+		if len(items) > maxNodeItems {
+			items = items[:1] // remove oldest element
+		}
+		b, err := serializer.SerializeSlice(&items)
+		if err != nil {
+			return err
+		}
+		m.bytes[pubSubItemsKey(host, name)] = b
+		return nil
+	})
 }
 
 func (m *Storage) FetchPubSubNodeItems(host, name string) ([]pubsubmodel.Item, error) {
-	return nil, nil
+	var b []byte
+	if err := m.inReadLock(func() error {
+		b = m.bytes[pubSubItemsKey(host, name)]
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	if b == nil {
+		return nil, nil
+	}
+	var items []pubsubmodel.Item
+	if err := serializer.DeserializeSlice(b, &items); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
 
 func (m *Storage) UpsertPubSubNodeAffiliation(affiliation *pubsubmodel.Affiliation, host, name string) error {
-	return nil
+	return m.inWriteLock(func() error {
+		var b []byte
+		var affiliations []pubsubmodel.Affiliation
+
+		b = m.bytes[pubSubAffiliationsKey(host, name)]
+		if b != nil {
+			if err := serializer.DeserializeSlice(b, &affiliations); err != nil {
+				return err
+			}
+		}
+		var updated bool
+		for i, aff := range affiliations {
+			if aff.JID == affiliation.JID {
+				affiliations[i] = *affiliation
+				updated = true
+				break
+			}
+		}
+		if !updated {
+			affiliations = append(affiliations, *affiliation)
+		}
+		b, err := serializer.SerializeSlice(&affiliations)
+		if err != nil {
+			return err
+		}
+		m.bytes[pubSubAffiliationsKey(host, name)] = b
+		return nil
+	})
 }
 
 func (m *Storage) FetchPubSubNodeAffiliations(host, name string) ([]pubsubmodel.Affiliation, error) {
-	return nil, nil
+	var b []byte
+	if err := m.inReadLock(func() error {
+		b = m.bytes[pubSubAffiliationsKey(host, name)]
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	if b == nil {
+		return nil, nil
+	}
+	var affiliations []pubsubmodel.Affiliation
+	if err := serializer.DeserializeSlice(b, &affiliations); err != nil {
+		return nil, err
+	}
+	return affiliations, nil
 }
 
 func pubSubNodeKey(host, name string) string {
 	return "pubSubNodes:" + host + ":" + name
+}
+
+func pubSubAffiliationsKey(host, name string) string {
+	return "pubSubAffiliations:" + host + ":" + name
+}
+
+func pubSubItemsKey(host, name string) string {
+	return "pubSubItems:" + host + ":" + name
 }
