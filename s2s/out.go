724910310419b43eafc79dcc82c807a80e986417
Miguel Ángel Ortuño <ortuman@gmail.com>
2019-12-23 12:22:19 +0100

    add context support

--- s2s/out.go
+++ s2s/out.go
@@ -10,11 +10,10 @@ import (
 	"fmt"
 	"sync/atomic"
 
-	"github.com/ortuman/jackal/runqueue"
-
 	streamerror "github.com/ortuman/jackal/errors"
 	"github.com/ortuman/jackal/log"
 	"github.com/ortuman/jackal/router"
+	"github.com/ortuman/jackal/runqueue"
 	"github.com/ortuman/jackal/session"
 	"github.com/ortuman/jackal/stream"
 	"github.com/ortuman/jackal/xmpp"
