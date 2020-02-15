094788276e7f9e1363d529e21b73ca084bf48fe5
ortuman <ortuman@gmail.com>
2020-02-15 13:54:34 +0100

    router refactoring

--- s2s/router/remote.go
+++ s2s/router/remote.go
@@ -7,25 +7,49 @@ package s2srouter
 
 import (
 	"context"
+	"sync"
 
+	"github.com/ortuman/jackal/router"
 	"github.com/ortuman/jackal/s2s"
+	"github.com/ortuman/jackal/stream"
 	"github.com/ortuman/jackal/xmpp"
 )
 
 type remoteRouter struct {
-	domain      string
-	localDomain string
-	outProvider s2s.OutProvider
+	remoteDomain string
+	localDomain  string
+	outProvider  s2s.OutProvider
+	mu           sync.RWMutex
+	outStm       stream.S2SOut
 }
 
 func newRemoteRouter(domain, localDomain string, outProvider s2s.OutProvider) *remoteRouter {
 	return &remoteRouter{
-		domain:      domain,
-		localDomain: localDomain,
-		outProvider: outProvider,
+		remoteDomain: domain,
+		localDomain:  localDomain,
+		outProvider:  outProvider,
 	}
 }
 
 func (r *remoteRouter) route(ctx context.Context, stanza xmpp.Stanza) error {
+	r.mu.RLock()
+	stm := r.outStm
+	r.mu.RUnlock()
+
+	if stm == nil {
+		r.mu.Lock()
+		stm = r.outStm
+		if stm == nil {
+			outStm, err := r.outProvider.GetOut(ctx, r.localDomain, r.remoteDomain)
+			if err != nil {
+				r.mu.Unlock()
+				return router.ErrFailedRemoteConnect
+			}
+			stm = outStm
+			r.outStm = stm
+		}
+		r.mu.Unlock()
+	}
+	stm.SendElement(ctx, stanza)
 	return nil
 }
