eb12fa50baede2ac3e303552beacb4fd9243ac8e
ortuman <ortuman@gmail.com>
2020-02-16 16:19:27 +0100

    router refactoring

--- s2s/router/remote.go
+++ s2s/router/remote.go
@@ -7,12 +7,10 @@ package s2srouter
 
 import (
 	"context"
-	"sync"
 
 	"github.com/ortuman/jackal/log"
 	"github.com/ortuman/jackal/router"
 	"github.com/ortuman/jackal/s2s"
-	"github.com/ortuman/jackal/stream"
 	"github.com/ortuman/jackal/xmpp"
 )
 
@@ -20,8 +18,6 @@ type remoteRouter struct {
 	remoteDomain string
 	localDomain  string
 	outProvider  s2s.OutProvider
-	mu           sync.RWMutex
-	outStm       stream.S2SOut
 }
 
 func newRemoteRouter(remoteDomain, localDomain string, outProvider s2s.OutProvider) *remoteRouter {
@@ -33,25 +29,11 @@ func newRemoteRouter(remoteDomain, localDomain string, outProvider s2s.OutProvid
 }
 
 func (r *remoteRouter) route(ctx context.Context, stanza xmpp.Stanza) error {
-	r.mu.RLock()
-	stm := r.outStm
-	r.mu.RUnlock()
-
-	if stm == nil {
-		r.mu.Lock()
-		stm = r.outStm
-		if stm == nil {
-			outStm, err := r.outProvider.GetOut(ctx, r.localDomain, r.remoteDomain)
-			if err != nil {
-				r.mu.Unlock()
-				log.Error(err)
-				return router.ErrFailedRemoteConnect
-			}
-			stm = outStm
-			r.outStm = stm
-		}
-		r.mu.Unlock()
+	outStm, err := r.outProvider.GetOut(ctx, r.localDomain, r.remoteDomain)
+	if err != nil {
+		log.Error(err)
+		return router.ErrFailedRemoteConnect
 	}
-	stm.SendElement(ctx, stanza)
+	outStm.SendElement(ctx, stanza)
 	return nil
 }
