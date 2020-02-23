51012d4b160633cb9ea2b94ccf435703b747c4fe
ortuman <ortuman@gmail.com>
2020-02-23 12:50:11 +0100

    router refactoring

--- s2s/outprovider.go
+++ s2s/outprovider.go
@@ -81,7 +81,8 @@ func (p *outProvider) Shutdown(ctx context.Context) error {
 
 func (p *outProvider) newOut(localDomain, remoteDomain string) *outStream {
 	tlsConfig := &tls.Config{
-		MaxVersion:   tls.VersionTLS11,
+		MinVersion:   tls.VersionTLS11,
+		MaxVersion:   tls.VersionTLS12,
 		ServerName:   remoteDomain,
 		Certificates: p.hosts.Certificates(),
 	}
