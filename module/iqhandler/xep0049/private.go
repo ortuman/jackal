97a6c07a33e97de59db859344323e3a294e81323
ortuman <ortuman@gmail.com>
2021-03-13 13:09:43 +0100

    XEP-0049: Private XML Storage

--- module/iqhandler/xep0049/private.go
+++ module/iqhandler/xep0049/private.go
@@ -18,15 +18,14 @@ import (
 	"context"
 	"strings"
 
-	stanzaerror "github.com/jackal-xmpp/stravaganza/errors/stanza"
-	xmpputil "github.com/ortuman/jackal/util/xmpp"
-
 	"github.com/jackal-xmpp/sonar"
 	"github.com/jackal-xmpp/stravaganza"
+	stanzaerror "github.com/jackal-xmpp/stravaganza/errors/stanza"
 	"github.com/ortuman/jackal/event"
 	"github.com/ortuman/jackal/log"
 	"github.com/ortuman/jackal/repository"
 	"github.com/ortuman/jackal/router"
+	xmpputil "github.com/ortuman/jackal/util/xmpp"
 )
 
 const privateNamespace = "jabber:iq:private"
