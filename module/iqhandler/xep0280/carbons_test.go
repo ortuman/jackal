afb5f4368e85d6fbb5ec5f5eae7e0e5b66f47358
ortuman <ortuman@gmail.com>
2021-03-30 14:32:53 +0200

    XEP-0280: Message Carbons

--- module/iqhandler/xep0280/carbons_test.go
+++ module/iqhandler/xep0280/carbons_test.go
@@ -19,6 +19,12 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/jackal-xmpp/stravaganza/jid"
+
+	"github.com/ortuman/jackal/event"
+
+	"github.com/ortuman/jackal/model"
+
 	"github.com/google/uuid"
 	"github.com/jackal-xmpp/sonar"
 	"github.com/jackal-xmpp/stravaganza"
@@ -161,12 +167,169 @@ func TestCarbons_Disable(t *testing.T) {
 
 func TestCarbons_SentCC(t *testing.T) {
 	// given
+	routerMock := &routerMock{}
+
+	var respStanzas []stravaganza.Stanza
+	routerMock.RouteFunc = func(ctx context.Context, stanza stravaganza.Stanza) error {
+		respStanzas = append(respStanzas, stanza)
+		return nil
+	}
+
+	jd0, _ := jid.NewWithString("ortuman@jackal.im/balcony", true)
+
+	resManagerMock := &resourceManagerMock{}
+	resManagerMock.GetResourcesFunc = func(ctx context.Context, username string) ([]model.Resource, error) {
+		return []model.Resource{
+			{JID: jd0, Context: map[string]string{carbonsEnabledCtxKey: "true"}},
+		}, nil
+	}
+
+	hMock := &hostsMock{}
+	hMock.IsLocalHostFunc = func(h string) bool {
+		return h == "jackal.im"
+	}
+
+	sn := sonar.New()
+	c := &Carbons{
+		router: routerMock,
+		resMng: resManagerMock,
+		hosts:  hMock,
+		sn:     sn,
+	}
+
+	remoteJID, _ := jid.NewWithString("jabber.org", true)
+
+	b := stravaganza.NewMessageBuilder()
+	b.WithAttribute("id", "i1234")
+	b.WithAttribute("from", "ortuman@jackal.im/yard")
+	b.WithAttribute("to", "noelia@jabber.org/balcony")
+	b.WithAttribute("type", "chat")
+	b.WithChild(
+		stravaganza.NewBuilder("body").
+			WithText("I'll give thee a wind.").
+			Build(),
+	)
+	msg, _ := b.BuildMessage(true)
+
 	// when
+	_ = c.Start(context.Background())
+	defer func() { _ = c.Stop(context.Background()) }()
+
+	_ = sn.Post(context.Background(), sonar.NewEventBuilder(event.S2SRouterStanzaRouted).
+		WithInfo(&event.S2SRouterEventInfo{
+			Target: remoteJID,
+			Stanza: msg,
+		}).
+		Build(),
+	)
+
 	// then
+	require.Len(t, respStanzas, 1)
+
+	routedMsg := respStanzas[0]
+
+	require.Equal(t, stravaganza.MessageName, routedMsg.Name())
+	require.Equal(t, "ortuman@jackal.im", routedMsg.Attribute(stravaganza.From))
+	require.Equal(t, "ortuman@jackal.im/balcony", routedMsg.Attribute(stravaganza.To))
+	require.NotNil(t, routedMsg.ChildNamespace("sent", carbonsNamespace))
 }
 
 func TestCarbons_ReceivedCC(t *testing.T) {
 	// given
+	routerMock := &routerMock{}
+
+	var respStanzas []stravaganza.Stanza
+	routerMock.RouteFunc = func(ctx context.Context, stanza stravaganza.Stanza) error {
+		respStanzas = append(respStanzas, stanza)
+		return nil
+	}
+
+	jd0, _ := jid.NewWithString("ortuman@jackal.im/balcony", true)
+	jd1, _ := jid.NewWithString("ortuman@jackal.im/hall", true)
+	jd2, _ := jid.NewWithString("ortuman@jackal.im/chamber", true)
+
+	resManagerMock := &resourceManagerMock{}
+	resManagerMock.GetResourcesFunc = func(ctx context.Context, username string) ([]model.Resource, error) {
+		return []model.Resource{
+			{JID: jd0, Context: map[string]string{carbonsEnabledCtxKey: "true"}},
+			{JID: jd1, Context: map[string]string{carbonsEnabledCtxKey: "false"}},
+			{JID: jd2, Context: map[string]string{carbonsEnabledCtxKey: "true"}},
+		}, nil
+	}
+
+	hMock := &hostsMock{}
+	hMock.IsLocalHostFunc = func(h string) bool {
+		return h == "jackal.im"
+	}
+
+	sn := sonar.New()
+	c := &Carbons{
+		router: routerMock,
+		resMng: resManagerMock,
+		hosts:  hMock,
+		sn:     sn,
+	}
+
+	b := stravaganza.NewMessageBuilder()
+	b.WithAttribute("id", "i1234")
+	b.WithAttribute("from", "noelia@jabber.org/balcony")
+	b.WithAttribute("to", "ortuman@jackal.im/chamber")
+	b.WithAttribute("type", "chat")
+	b.WithChild(
+		stravaganza.NewBuilder("body").
+			WithText("I'll give thee a wind.").
+			Build(),
+	)
+	msg, _ := b.BuildMessage(true)
+
 	// when
+	_ = c.Start(context.Background())
+	defer func() { _ = c.Stop(context.Background()) }()
+
+	_ = sn.Post(context.Background(), sonar.NewEventBuilder(event.C2SRouterStanzaRouted).
+		WithInfo(&event.C2SRouterEventInfo{
+			Targets: []jid.JID{*jd2},
+			Stanza:  msg,
+		}).
+		Build(),
+	)
+
 	// then
+	require.Len(t, respStanzas, 1)
+
+	routedMsg := respStanzas[0]
+
+	require.Equal(t, stravaganza.MessageName, routedMsg.Name())
+	require.Equal(t, "ortuman@jackal.im", routedMsg.Attribute(stravaganza.From))
+	require.Equal(t, "ortuman@jackal.im/balcony", routedMsg.Attribute(stravaganza.To))
+	require.NotNil(t, routedMsg.ChildNamespace("received", carbonsNamespace))
+}
+
+func TestCarbons_PreRoute(t *testing.T) {
+	// given
+	c := &Carbons{
+		sn: sonar.New(),
+	}
+
+	b := stravaganza.NewMessageBuilder()
+	b.WithAttribute("from", "noelia@jackal.im/yard")
+	b.WithAttribute("to", "ortuman@jackal.im/balcony")
+	b.WithChild(
+		stravaganza.NewBuilder("body").
+			WithText("I'll give thee a wind.").
+			Build(),
+	)
+	b.WithChild(
+		stravaganza.NewBuilder("private").
+			WithAttribute(stravaganza.Namespace, carbonsNamespace).
+			Build(),
+	)
+	msg, _ := b.BuildMessage(true)
+
+	// when
+	msg, err := c.PreRouteMessage(context.Background(), msg)
+
+	// then
+	require.Nil(t, err)
+	require.Nil(t, msg.ChildNamespace("private", carbonsNamespace))
 }
