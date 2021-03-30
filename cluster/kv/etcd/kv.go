a17545e09fedc3451fc1338e85a1669f3b126984
ortuman <ortuman@gmail.com>
2021-03-30 18:19:28 +0200

    XEP-0280: Message Carbons

--- cluster/kv/etcd/kv.go
+++ cluster/kv/etcd/kv.go
@@ -160,7 +160,7 @@ func (k *KV) refreshLeaseTTL() {
 				log.Warnf("Failed to perform KV lease keepalive: %v", err)
 
 				k.leaseRefreshTries++
-				if k.leaseRefreshTries == maxLeaseRefreshTries || errors.Is(err, context.Canceled) {
+				if k.leaseRefreshTries == maxLeaseRefreshTries || errors.Is(err, context.DeadlineExceeded) {
 					// almost certainly KV lease has expired... shutdown process to avoid a split-brain scenario
 					log.Errorw("Unable to refresh KV lease keepalive...")
 					shutdownProcess()
