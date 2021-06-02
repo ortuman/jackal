ba15cb36cc3a6a6d75a5ab80868b0bd02ca2d977
ortuman <ortuman@gmail.com>
2021-06-02 12:40:51 +0200

    Add support for XEP-0198: Stream Management

--- pkg/c2s/in.go
+++ pkg/c2s/in.go
@@ -346,14 +346,11 @@ func (s *inC2S) handleSessionResult(elem stravaganza.Element, sErr error) {
 	s.rq.Run(func() {
 		defer close(doneCh)
 
-		log.Debugf("HANDLING SESSION RESULT...")
-
 		ctx, cancel := s.requestContext()
 		defer cancel()
 
 		switch {
 		case sErr == nil && elem != nil:
-			log.Debugf("HANDLING ELEMENT...")
 			err := s.handleElement(ctx, elem)
 			if err != nil {
 				log.Warnw("Failed to process incoming C2S session element", "error", err, "id", s.id)
@@ -361,7 +358,6 @@ func (s *inC2S) handleSessionResult(elem stravaganza.Element, sErr error) {
 			}
 
 		case sErr != nil:
-			log.Debugf("HANDLING SESSION ERROR: %s... STATE: %d", sErr, s.getState())
 			s.handleSessionError(ctx, sErr)
 		}
 	})
